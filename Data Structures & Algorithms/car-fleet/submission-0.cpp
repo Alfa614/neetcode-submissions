4ea7994473fc735639896d900c5c7994aed09687
Alfa614 <111535364+Alfa614@users.noreply.github.com>
2026-07-28 18:52:40 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<pair<int,int>> cars;
+        for(int i=0;i<speed.size();i++)
+            cars.push_back({position[i],speed[i]});
+        
+        sort(cars.begin(),cars.end(),greater<pair<int,int>>());
+        stack<double> st;
+
+        for(auto &car: cars){
+            double time = (target-car.first)/car.second;
+            if(st.empty() || time>st.top())
+                st.push(time);
+        }
+        return st.size();
+    }
+};
