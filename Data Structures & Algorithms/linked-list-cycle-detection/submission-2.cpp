befd2281fac8debd2e98bab016461170bfdee472
Alfa614 <111535364+Alfa614@users.noreply.github.com>
2026-08-01 06:34:56 +0530

    Add: linked-list-cycle-detection - submission-2

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-2.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        unordered_set<ListNode*> visited;
+        ListNode* curr = head;
+        while(curr != nullptr){
+            if(visited.count(curr))
+                return true;
+            visited.insert(curr);
+            curr = curr->next;
+        }
+        return false;
+    }
+};
